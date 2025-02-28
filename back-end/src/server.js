e774ab36d3ff98ac289fd3d07e4ea3385c6a0ee8
VictorPedroza <victor242206@gmail.com>
2025-02-28 11:46:43 -0300

    First Commit

---
+++ back-end/src/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        message: "API is Running 🚀"
+    })
+});
+
+const PORT = process.env.PORT;
+const NODE_ENV = process.env.NODE_ENV;
+
+app.listen(PORT, () => {
+    console.log("API is Running on:", NODE_ENV);
+});
